f65b31fdaed9bbe192f04fd2d2a14ce8aae1102e
huliua <1148247580@qq.com>
2024-11-25 18:22:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import BaseLayout from '@/layout/BaseLayout/index.vue'
+</script>
+
+<template>
+  <div id="app">
+    <base-layout />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/layout/BaseLayout/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+
+defineComponent({
+  name: 'BaseLayout',
+})
+</script>
+
+<template>
+  <div id="base-layout">
+    <a-layout>
+      <a-layout-header>Header</a-layout-header>
+      <a-layout>
+        <a-layout-sider>Sider</a-layout-sider>
+        <a-layout-content>Content</a-layout-content>
+      </a-layout>
+      <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped></style>
